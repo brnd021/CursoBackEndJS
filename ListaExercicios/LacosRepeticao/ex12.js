62540499c9c9878918557138276f240914a7f2b6
brnd021 <bernardocustodio686@gmail.com>
2025-10-22 16:22:11 -0300

    Refactor loop structure and enhance output messages for number comparison exercise

--- ListaExercicios/LacosRepeticao/ex12.js
+++ ListaExercicios/LacosRepeticao/ex12.js
@@ -3,7 +3,7 @@ let maior;
 let menor;
 let primeiroNumero = true;
 
-for (;;) {
+while (primeiroNumero) {
     let numero = parseInt(prompt("Digite um número:"));
     if ( numero === 0 ) break;
     if ( primeiroNumero ) {
@@ -11,8 +11,12 @@ for (;;) {
         menor = numero;
         primeiroNumero = false;
     }   else {
-    if ( numero > maior ) maior = numero;
-    if ( numero < menor) menor = numero;
+            if ( numero > maior ) maior = numero;
+            if ( numero < menor) menor = numero;
     }
 }
-resultado.innerHTML = `O maior número digitado foi ${maior} e o menor foi ${menor}`;
+if (primeiroNumero === true) {
+    resultado.innerHTML = `Nenhum número diferente de zero foi digitado.`;
+} else {
+    resultado.innerHTML = `O maior número digitado foi **${maior}** e o menor foi **${menor}**`;
+}
