49992396b09f23988e2bdf0f052fa0f0adb39604
brnd021 <bernardocustodio686@gmail.com>
2025-10-22 16:19:19 -0300

    Add initial object exercises with methods for managing data

---
+++ ListaExercicios/Objetos/ex11.js
@@ -0,0 +1,31 @@
+biblioteca = {
+    livros: [
+    {
+        autor: 'Bill Gates', 
+        titulo: 'O Caminho à Frente',
+        ano: 2014
+    },
+    { 
+        autor: 'Steve Jobs',
+        titulo: 'Walter Isaacson',
+        ano:2020
+    },
+    {
+        autor: 'Suzanne Collins',
+        titulo:  'A Esperança: O Último Livro de Jogos Vorazes',
+        ano:2015
+    },],
+    adicionarLivro: function(livro){
+        this.livros.push(livro);
+    },
+    buscarPorAutor: function(nomeAutor){
+        let livrosDoAutor = this.livros.filter(p=>p.autor === nomeAutor);
+        if (livrosDoAutor.length>0) console.log(`Livros encontrados para o autor: "${nomeAutor}".`,"\n",livrosDoAutor)
+            else console.log (`Nenhum livro encontrado para o autor: "${nomeAutor}".`)
+    },
+    listarLivros: function(){
+        console.log(this.livros);
+    }
+
+}
+
