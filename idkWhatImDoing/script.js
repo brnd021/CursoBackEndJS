75209534c6c2a9fc57c7c07fbbb682236ee609c5
brnd021 <bernardocustodio686@gmail.com>
2025-10-23 16:42:36 -0300

    Refactor HTML and JavaScript for book library: update table structure and improve data handling

--- idkWhatImDoing/script.js
+++ idkWhatImDoing/script.js
@@ -1,30 +1,54 @@
+const botao = document.getElementById("botao");
+const bodyTabela = document.getElementById("tabela");
+
 const biblioteca = [
     { 
         autor: 'Bill Gates', 
         titulo: 'O Caminho à Frente',
-        ano: 2014
+        ano: 1995
     }, 
     { 
-        autor: 'Steve Jobs',
-        titulo: 'Walter Isaacson',
-        ano:2020
+        autor: 'Walter Isaacson' ,
+        titulo: 'Steve Jobs',
+        ano: 2022
     },
     {
         autor: 'Suzanne Collins',
         titulo:  'A Esperança: O Último Livro de Jogos Vorazes',
-        ano:2015
+        ano: 2011
     }
 ]
 
 function gerarTabela(){
-    for(let i = 0; i <= biblioteca.length; i++){
-    const autor = document.getElementById(`autor ${i}`);
-    const titulo = document.getElementById(`titulo ${i}`);
-    const ano = document.getElementById(`ano ${i}`);
+    let html ='';
+    biblioteca.forEach((livro,p)=>{
+    html += `
+        <tr>
+            <th scope="row">${p + 1}</th>
+            <td>${livro.autor}</td>
+            <td>${livro.titulo}</td>
+            <td>${livro.ano}</td>
+        </tr>`
+    })
+    bodyTabela.innerHTML = html;
+};
 
-    autor.textContent = biblioteca[i].autor;
-    titulo.textContent = biblioteca[i].titulo;
-    ano.textContent = biblioteca[i].ano;
-}
-}
 gerarTabela();
+
+botao.addEventListener('click', function() {
+    const novoAutorInput = document.getElementById("Novo-Autor");
+    const novoTituloInput = document.getElementById("Novo-Titulo");
+    const novoAnoInpunt = document.getElementById("Novo-Ano");
+
+    const autor = novoAutorInput.value;
+    const titulo = novoTituloInput.value;
+    const ano = parseInt(novoAnoInpunt.value);
+    if(autor && titulo && !isNan(ano)){
+        biblioteca.push({
+        autor: autor,
+        titulo: titulo,
+        ano: ano
+        });
+        gerarTabela();
+    }else alert("Insira os dados corretamente:");
+});
