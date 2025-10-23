b31c65869b758bbbcbc764660ab7a0cf556bef62
brnd021 <bernardocustodio686@gmail.com>
2025-10-22 21:54:45 -0300

    Add HTML and JavaScript files for calculator functionality

---
+++ mod02Log/logProg/calculadora.js
@@ -0,0 +1,24 @@
+function calcular(num,num2){
+    let operacao = parseInt(prompt("Qual operacao deseja realizar? add(1) sub (2) mult(3) divi(4)"))
+    if (isNaN(operacao)){
+        alert("Erro de operacao")
+    }
+    else{
+        switch (operacao){
+        case 1:
+            return num + num2;
+        case 2: 
+            return num - num2;
+        case 3:
+            return num * num2;
+        case 4:
+            if(num2 == 0) alert("Divisao por zero")
+            else return num / num2;
+        }
+    }
+};
+const resultado = document.getElementById('resultado');
+const num = parseFloat(prompt("Digite um numero"));
+const num2 = parseFloat(prompt("Digite mais um numero")); 
+const conta = calcular(num,num2);
+resultado.innerHTML = `${conta}`;
