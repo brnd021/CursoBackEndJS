3839478e382610f87e1f1136bc2c6d00003d9f77
brnd021 <bernardocustodio686@gmail.com>
2025-10-23 17:22:23 -0300

    Enhance book library interface: improve form layout, update table styles, and fix JavaScript variable references

--- idkWhatImDoing/script.js
+++ idkWhatImDoing/script.js
@@ -2,22 +2,10 @@ const botao = document.getElementById("botao");
 const bodyTabela = document.getElementById("tabela");
 
 const biblioteca = [
-    { 
-        autor: 'Bill Gates', 
-        titulo: 'O Caminho à Frente',
-        ano: 1995
-    }, 
-    { 
-        autor: 'Walter Isaacson' ,
-        titulo: 'Steve Jobs',
-        ano: 2022
-    },
-    {
-        autor: 'Suzanne Collins',
-        titulo:  'A Esperança: O Último Livro de Jogos Vorazes',
-        ano: 2011
-    }
-]
+    { autor: 'Bill Gates', titulo: 'O Caminho à Frente',ano: 1995}, 
+    { autor: 'Walter Isaacson' , titulo: 'Steve Jobs', ano: 2022},
+    { autor: 'Suzanne Collins', titulo:  'A Esperança: O Último Livro de Jogos Vorazes', ano: 2011}
+];
 
 function gerarTabela(){
     let html ='';
@@ -35,15 +23,16 @@ function gerarTabela(){
 
 gerarTabela();
 
-botao.addEventListener('click', function() {
-    const novoAutorInput = document.getElementById("Novo-Autor");
-    const novoTituloInput = document.getElementById("Novo-Titulo");
-    const novoAnoInpunt = document.getElementById("Novo-Ano");
+botao.addEventListener('click', function(e) {
+    e.preventDefault();
+    const novoAutorInput = document.getElementById("novo-autor");
+    const novoTituloInput = document.getElementById("novo-titulo");
+    const novoAnoInpunt = document.getElementById("novo-ano");
 
     const autor = novoAutorInput.value;
     const titulo = novoTituloInput.value;
     const ano = parseInt(novoAnoInpunt.value);
-    if(autor && titulo && !isNan(ano)){
+    if(autor && titulo && !isNaN(ano)){
         biblioteca.push({
         autor: autor,
         titulo: titulo,
