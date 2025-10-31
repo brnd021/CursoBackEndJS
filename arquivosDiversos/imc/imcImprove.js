42be4bb499ba01860b3a25022f2d4246788f53b2
brnd021 <bernardocustodio686@gmail.com>
2025-10-31 19:57:27 -0300

    Add IMC calculator functionality with responsive design and classification logic

---
+++ arquivosDiversos/imc/imcImprove.js
@@ -0,0 +1,41 @@
+import imcSpecs from './imcSpecs.js'
+
+function calculaIMC(peso,h){
+    return peso / Math.Pow(h,2)
+}
+
+function classificaIMC(idade,sexo,peso,h){
+    let imc = calculaIMC(peso,h);
+    let idadeReal = idade < 10 ? 5 : idade < 15 ? 10 : 15;
+    let tabela;
+
+    if (idade < 18) {
+        if (sexo === "M") tabela = imcSpecs.menino.idade[idadeReal];
+        else tabela = imcSpecs.menina.idade[idadeReal];
+    } 
+    else if (idade < 60) {
+        tabela = imcSpecs.adulto;
+    } 
+    else {
+        tabela = imcSpecs.idoso;
+    }
+
+    const alvo = tabela.find(e => imc <= e.imc);
+    return alvo ? alvo.classifica : tabela[tabela.length - 1].classifica;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.getElementById('enviar');
+    const resultado = document.getElementById('resultado');
+
+    
+    botao.addEventListener('click', function() {
+        const peso = parseFloat(document.getElementById('peso').value);
+        const altura = parseFloat(document.getElementById('altura').value);
+        const idade = parseInt(document.getElementById('idade').value);
+        const sexo = document.querySelector('input[name="sexo"]:checked').value.toUpperCase();
+        let IMC = calculaIMC(peso,altura);
+        let classificacao = classificaIMC(idade,sexo,peso,altura);
+        resultado.textContent = `Seu IMC é ${IMC.toFixed(1)} e voce esta com ${classificacao}`
+    });
+});
