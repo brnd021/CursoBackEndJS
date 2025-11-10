43d078acd29d65636af35ae652bf27d764ebcd85
brnd021 <bernardocustodio686@gmail.com>
2025-11-10 10:17:22 -0300

    Refactor and reorganize timer, cadastro, and prime number scripts; add new implementations for Timer and Cadastro functionalities

--- mod02Log/logProg/Slide/9NumerosPrimos/NumeroPrimo.js
+++ mod02Log/logProg/Slide/9NumerosPrimos/NumeroPrimo.js
@@ -1,15 +1,16 @@
-let i = 1;
 const numeros =[];
-const numerosPrimos = [1];
-for(;i<=1000;i++) numeros.push(i)
+const numerosPrimos = [];
+
+for(let i = 2;i<=1000;i++) numeros.push(i)
+
 for(let e of numeros){
     let contador = 0;
-    let divi = 0;
+    let divi = 1;
     while (divi <= e) {
         if (e%divi === 0) contador++;
         divi++
     }
     if(contador == 2)numerosPrimos.push(e)
 }    
-console.log(numeros) 
+
 console.table(numerosPrimos)
