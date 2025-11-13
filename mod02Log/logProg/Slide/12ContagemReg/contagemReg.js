850b6c2baa08d583cfa48f0492d4955062c8f800
brnd021 <bernardocustodio686@gmail.com>
2025-11-13 18:14:19 -0300

    Add IMC and body fat percentage calculator; implement HTML structure, CSS styling, and JavaScript functionality

---
+++ mod02Log/logProg/Slide/12ContagemReg/contagemReg.js
@@ -0,0 +1,16 @@
+let i = 10
+const regre = setInterval(()=>{
+    if(i===0){
+        clearInterval(regre);
+        console.log("Tempo esgotado!")
+    }
+    else{
+    console.log(i);
+    i--;
+    }
+},1000);
+
+
+const timeout = setTimeout(()=>{
+    console.log("Reiniciando contagem");
+},13000);
